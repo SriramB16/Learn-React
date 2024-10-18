6c54e8c2152fc23093aa7071c3a7c697bb469275
Sriram <136231040+SriramB16@users.noreply.github.com>
2024-10-18 20:04:24 +0530

    States have been added

--- Thinking_in_React/src/components/FilterableProductTable.jsx
+++ Thinking_in_React/src/components/FilterableProductTable.jsx
@@ -1,7 +1,9 @@
+import { useState } from 'react'
 import SearchBar from "./SearchBar.jsx";
 import ProductTable from "./ProductTable.jsx";
 
 export default function FilterableProductTable({products}) {
+    const [] = useState();
   return (
     <>
       <SearchBar />
