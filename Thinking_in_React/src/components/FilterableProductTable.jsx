124a9136ff4ed083e73ab9a25dffaf7c4ae36b53
Sriram <136231040+SriramB16@users.noreply.github.com>
2024-10-19 07:42:39 +0530

    code completed

--- Thinking_in_React/src/components/FilterableProductTable.jsx
+++ Thinking_in_React/src/components/FilterableProductTable.jsx
@@ -3,11 +3,14 @@ import SearchBar from "./SearchBar.jsx";
 import ProductTable from "./ProductTable.jsx";
 
 export default function FilterableProductTable({products}) {
-    const [] = useState();
+    const [filterText, setFilterText] = useState('');
+    const [inStockOnly, setInStockOnly] = useState(false);
   return (
-    <>
-      <SearchBar />
-      <ProductTable products={products}/>
+    <> 
+      <SearchBar filterText={filterText} inStockOnly={inStockOnly}  onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly}/>
+      <ProductTable products={products} filterText={filterText} inStockOnly={inStockOnly}/>
+
     </>
   );
 }
--- Thinking_in_React/src/components/SearchBar.jsx
+++ Thinking_in_React/src/components/SearchBar.jsx
@@ -1,9 +1,9 @@
-export default function SearchBar(){
+export default function SearchBar({filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange}){
     return(
         <div>
-            <input type="text" placeholder="Search..."/>
+            <input type="text" placeholder="Search..." value={filterText}  onChange={(e) => onFilterTextChange(e.target.value)}/>
             <label>
-                <input type="checkbox" /> Only show products in stock
+                <input type="checkbox" checked={inStockOnly} onChange={(e) => onInStockOnlyChange(e.target.checked)}/> Only show products in stock
             </label>
         </div>
     )
